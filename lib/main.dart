e5b2ac199f5088c80a257c930b63146a9a71eb96
parshuXX <parshu.nepal01@gmail.com>
2023-01-10 09:54:33 +0545

    learned icons on dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,12 @@
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:http_app/bloc/pixabay_image/image_cubit.dart';
+import 'package:http_app/bloc/posts/posts_bloc.dart';
+import 'package:http_app/utils/env.dart';
+import 'package:http_app/widget/hastag_widget.dart';
 
-import 'screen/homepage.dart';
-import 'package:flutter/material.dart';
+import 'navigation/route_generator.dart';
 
 main() {
   runApp(const MyApp());
@@ -10,23 +14,30 @@ main() {
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
-  @override
-  Widget build(BuildContext context) {
-    return const MaterialApp(
-      title: "Api App",
-      home: Homepage(),
-    );
-  }
-}
-
-class MyWidget extends StatelessWidget {
-  const MyWidget({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text("welcome"),
+    return HashtagWidget(
+      hashtag: "#happynewyear",
+      env: Env.devEnv,
+      child: MultiBlocProvider(
+        providers: [
+          BlocProvider<ImageCubit>(create: (ctx) => ImageCubit()),
+          BlocProvider<PostBloc>(create: (ctx) => PostBloc()),
+        ],
+        child: MaterialApp(
+          title: "Api App",
+          initialRoute: "images_bloc",
+          onGenerateRoute: customRouteGenerator,
+          theme: ThemeData(
+            fontFamily: "Montserrat",
+            textTheme: TextTheme(
+              headline6:
+                  GoogleFonts.sevillana(fontSize: 18, color: Color(0xff1f1f1f)),
+            ),
+          ),
+          // home: Homepage(),
+        ),
       ),
     );
   }
